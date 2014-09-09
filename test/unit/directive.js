549023b2bde51a1d5e11787cea7849ce349dd89d
Gilles <gilles.cochez@gmail.com>
2014-09-09 12:21:15 +0100

    directive UT placeholder

---
+++ test/unit/directive.js
@@ -0,0 +1,13 @@
+describe("angular audio sprite directive", function() {
+
+    var $rootScope;
+    var $compile;
+
+    beforeEach(module("ngAudioSprite.directive"));
+
+    beforeEach(inject(function(_$rootScope_, _$compile_) {
+        $rootScope = _$rootScope_;
+        $compile = _$compile_;
+    }));
+
+});

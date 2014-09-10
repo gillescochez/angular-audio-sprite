1833e1a757f28c9c92473bbfa457c9343be7ece1
gillescochez <git@csurl.eu>
2014-09-10 23:13:30 +0100

    js files build order update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,9 @@
 module.exports = function(grunt) {
 
     var sources = [
-        "source/*.js",
+        "source/service.js",
+        "source/directive.js",
+        "source/module.js"
     ];
 
     var src = "<%= pkg.name %>.js";
--- angular-audio-sprite.js
+++ angular-audio-sprite.js
@@ -1,3 +1,84 @@
+angular.module("ngAudioSprite.service", []).factory("audioSprite", ["$http", function($http) {
+
+    var audioSprite = {
+
+        id: "",
+        config: {},
+
+        play: function(id) {
+            this.id = id;
+            notifyObservers("id");
+        },
+
+        stop: function() {
+            this.id = "";
+            notifyObservers("id");
+        },
+
+        spritemap: function(spritemap) {
+            this.config.spritemap = spritemap;
+        },
+
+        configure: function(config) {
+            if (config && config.resources && config.spritemap) {
+                this.config = config;
+                notifyObservers("config");
+            } else {
+                throw "Invalid configuration object";
+            }
+        },
+
+        load: function(file) {
+
+            var self = this;
+
+            $http.get(file).success(function(data) {
+                self.config = data;
+                self.config.path = getPath(file);
+                notifyObservers("config");
+            }).error(function() {
+                throw "Failed to retrieve audio sprite configuration file: " + file;
+            })
+        },
+
+        observe: addObserver,
+        removeObservers: removeObservers
+
+    };
+
+    var observers = {};
+
+    function getPath(url) {
+        var parts = url.split("/");
+        parts.splice(parts.length - 1, 1);
+        return parts.join("/") + "/";
+    }
+
+    function notifyObservers(prop) {
+        angular.forEach(observers[prop], function(observer){
+            observer.callback.call(observer.scope);
+        });
+    }
+
+    function addObserver(prop, callback, scope) {
+
+        if (!observers[prop]) {
+            observers[prop] = [];
+        }
+
+        observers[prop].push({
+            callback: callback,
+            scope: scope
+        });
+    }
+
+    function removeObservers() {
+        observers = {};
+    }
+
+    return audioSprite;
+
+}]);
 angular.module("ngAudioSprite.directive", []).directive("ngAudioSprite", ["audioSprite", function(audioSprite) {
 
     var player;
@@ -146,84 +227,3 @@ angular.module("ngAudioSprite", [
     "ngAudioSprite.service",
     "ngAudioSprite.directive"
 ]);
-angular.module("ngAudioSprite.service", []).factory("audioSprite", ["$http", function($http) {
-
-    var audioSprite = {
-
-        id: "",
-        config: {},
-
-        play: function(id) {
-            this.id = id;
-            notifyObservers("id");
-        },
-
-        stop: function() {
-            this.id = "";
-            notifyObservers("id");
-        },
-
-        spritemap: function(spritemap) {
-            this.config.spritemap = spritemap;
-        },
-
-        configure: function(config) {
-            if (config && config.resources && config.spritemap) {
-                this.config = config;
-                notifyObservers("config");
-            } else {
-                throw "Invalid configuration object";
-            }
-        },
-
-        load: function(file) {
-
-            var self = this;
-
-            $http.get(file).success(function(data) {
-                self.config = data;
-                self.config.path = getPath(file);
-                notifyObservers("config");
-            }).error(function() {
-                throw "Failed to retrieve audio sprite configuration file: " + file;
-            })
-        },
-
-        observe: addObserver,
-        removeObservers: removeObservers
-
-    };
-
-    var observers = {};
-
-    function getPath(url) {
-        var parts = url.split("/");
-        parts.splice(parts.length - 1, 1);
-        return parts.join("/") + "/";
-    }
-
-    function notifyObservers(prop) {
-        angular.forEach(observers[prop], function(observer){
-            observer.callback.call(observer.scope);
-        });
-    }
-
-    function addObserver(prop, callback, scope) {
-
-        if (!observers[prop]) {
-            observers[prop] = [];
-        }
-
-        observers[prop].push({
-            callback: callback,
-            scope: scope
-        });
-    }
-
-    function removeObservers() {
-        observers = {};
-    }
-
-    return audioSprite;
-
-}]);

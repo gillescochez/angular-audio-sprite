77a48dcab9de6a4f8df3fcf3e3b782f0217bd246
gillescochez <git@csurl.eu>
2014-09-11 10:31:15 +0100

    mute/unmute/pause/volume demo

--- app/demo.js
+++ app/demo.js
@@ -2,6 +2,23 @@ angular.module("demo", ["ngAudioSprite"]).controller("main", ["$scope", "audioSp
 
     $scope.list = ["round1", "round2", "round3", "round4", "round5", "round6", "round-final"];
 
+    $scope.volumeValue = 80;
+    $scope.volume = function() {
+        audioSprite.volume($scope.volumeValue / 100);
+    };
+
+    $scope.mute = function() {
+        if (audioSprite.muted) {
+            audioSprite.unmute();
+        } else {
+            audioSprite.mute();
+        }
+    };
+
+    $scope.pause = function() {
+        audioSprite.pause();
+    };
+
     $scope.play = function(id) {
         audioSprite.play(id);
     };
